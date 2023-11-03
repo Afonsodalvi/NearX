97944af4bddb7d9257c478143e51a1d184f5e818
Afonsodalvi <afonso.dalvi@hotmail.com>
2023-11-03 09:12:06 -0300

    loop-1

--- src/aula5/4ExercioCompleto.sol
+++ src/aula5/4ExercioCompleto.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 contract ExercicioLoja {
+    
     enum Status //inserindo o status do usuario
     {
         PENDING, //0
@@ -20,6 +21,34 @@ contract ExercicioLoja {
 
     uint256 minimumValue = 1 ether;
     string[] Product = ["Iphone 15", "MacBook", "AppleWatch"]; //os produtos da loja
+    
+    ///estrutura dos produtos
+    struct iphone{
+        uint iphoneSolds;
+        uint stock;
+    }
+
+    iphone public Iphone;
+
+    struct mac{
+        uint macSolds;
+        uint stock;
+    }
+
+    mac public Mac;
+
+    struct watch{
+        uint watchSolds;
+        uint stock;
+    }
+
+    watch public Watch;
+
+    constructor(uint _stockIphone, uint _stockWacth, uint _stockMac){ //definimos as quantidades maximas no estoque
+        Iphone.stock = _stockIphone;
+        Mac.stock = _stockMac;
+        Watch.stock = _stockWacth;
+    }
 
     function get() external view returns (Status) {
         //retorna em qual status esta
@@ -50,19 +79,34 @@ contract ExercicioLoja {
 
     function executePurchase(string memory opcaoNum) external payable checkProduct(opcaoNum) {
         if (!approved[msg.sender]) revert("not approved");
+
         if (compareStrings(opcaoNum, Product[0])) {
             require(msg.value == 1 ether, "incorrect iphone value");
+
+            for (uint256 i = 0; i < 1; i++) {//pega a quantidade
+            Iphone.iphoneSolds++; //incrementa e sendo o ID novo
+        }
+
             balance[msg.sender] += msg.value;
             status = Status.PURCHASE;
         }
         if (compareStrings(opcaoNum, Product[1])) {
             require(msg.value == 2 ether, "incorrect MacBook value");
+
+            for (uint256 i = 0; i < 1; i++) {//pega a quantidade
+            Mac.macSolds++; //incrementa e sendo o ID novo
+            }
+
             balance[msg.sender] += msg.value;
             status = Status.PURCHASE;
         }
         if (compareStrings(opcaoNum, Product[2])) {
             require(msg.value == 5_000_000_000_000_000_000 wei, "incorrect AppleWatch value"); //https://eth-converter.com/
             //equivalente a 0.5 ether
+            for (uint256 i = 0; i < 1; i++) {//pega a quantidade
+            Watch.watchSolds++; //incrementa e sendo o ID novo
+            }
+
             balance[msg.sender] += msg.value;
             status = Status.PURCHASE;
         }
@@ -86,3 +130,4 @@ contract ExercicioLoja {
         _;
     }
 }
+
